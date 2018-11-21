17bcb8a68bc5d564ed8e783426beab3ed99126ce
Jalerson Lima <jalerson@gmail.com>
2018-11-21 20:50:59 +0100

    Remove files

--- webhooks.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports.github = async (event, context) => {
-  console.log("EVENT: "+ console.log(JSON.stringify(event, null, 4)));
-  console.log("CONTEXT: "+ console.log(JSON.stringify(context, null, 4)));
-
-  return { statusCode: 200 };
-};
